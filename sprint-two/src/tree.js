40d82a993c6bf2bd28ade0754ab7e808edaedb87
Kamran Masood <kam1088@gmail.com>
2022-02-25 12:32:28 -0800

    completed tree.js up to .contains method, break for lunch

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,26 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      // this.contains(this.children[i]);
+      this.children[i].contains(target);
+    }
+  }
 };
 
 
