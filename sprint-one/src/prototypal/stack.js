3f121d5ee7858c918ec99138a6bd174414e8980d
Kamran Masood <kam1088@gmail.com>
2022-02-24 17:26:11 -0800

    completed prototypal implementation of stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.position = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var position = ++this.position;
+  this.storage[position] = value;
+};
 
+stackMethods.pop = function() {
+  var position = this.position;
+  if (position === 0) {
+    return;
+  }
+  var popped = this.storage[position];
+  delete popped;
+  this.position--;
+  return popped;
+};
+
+stackMethods.size = function() {
+  return this.position;
+};
