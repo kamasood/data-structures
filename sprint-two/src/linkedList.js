419297be276cf889f6cab1a23bc595fdf4b598b5
Kamran Masood <kam1088@gmail.com>
2022-02-25 12:31:56 -0800

    completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,35 @@
 var LinkedList = function() {
   var list = {};
+
+  var storage = {}; // store
+  var length = 0;
+
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    list.head === null ? list.head = newNode : list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+    while (temp !== null) {
+      if (temp.value === target) {
+        return true;
+      } else {
+        temp = temp.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -23,6 +43,8 @@ var Node = function(value) {
 
   return node;
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
