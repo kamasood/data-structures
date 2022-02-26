f7f9bdee5d7f26e46eaf7342b8b85350f5a5ed6a
Kamran Masood <kam1088@gmail.com>
2022-02-26 11:22:44 -0800

    completed hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,22 +7,57 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var thisTuple = [k, v];
+  var arrayOfTuples = this._storage.get(index);
 
-  // each index should have an array of tuples
+  if (arrayOfTuples === undefined) {
+    this._storage.set(index, []);
+  }
+
+  arrayOfTuples = this._storage.get(index);
+
+  var exists = false;
+
+  arrayOfTuples.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      exists = true;
+      return;
+    }
+  });
+
+  if (!exists) {
+    arrayOfTuples.push(thisTuple);
+  }
+
+  console.log(arrayOfTuples);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arrayOfTuples = this._storage.get(index);
+  var result;
 
-  // lookup _storage at index
-
-  // might need to loop through multiple arrays at storage location
+  arrayOfTuples.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      result = tuple[1];
+      return;
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arrayOfTuples = this._storage.get(index);
+  var result;
 
-  //
+  arrayOfTuples.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      arrayOfTuples.splice(arrayOfTuples.indexOf(tuple), 1);
+      return;
+    }
+  });
 };
 
 
